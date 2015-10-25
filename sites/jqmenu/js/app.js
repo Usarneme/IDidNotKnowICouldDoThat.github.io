c99041b7b7fc4a0d107fb3e29013d76dbcd93141
Tom from Tomayo Web Design, Portland OR USA <tomayo.web.design@gmail.com>
2015-10-25 08:23:37 -0700

    Added lightbox image gallery to the Home Page

--- sites/jqmenu/js/app.js
+++ sites/jqmenu/js/app.js
@@ -1,6 +1,4 @@
-//Problem: It look gross in smaller browser widths and small devices
-//Solution: To hide the text links and swap them out with a more appropriate navigation
-
+// Menu Section
 //Create a select and append to #menu
 var $select = $("<select></select>");
 $("#menu").append($select);
@@ -28,9 +26,43 @@ $select.change(function(){
   //Go to select's location
   window.location = $select.val();
 });
+// END of menu section 
+
+// IMAGE GALLERY section 
 
 
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
 
+//An image to overlay
+$overlay.append($image);
+
+//Add overlay
+$("body").append($overlay);
+  //A caption
+
+//Capture the click event on a link to an image
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+  
+  //Show the overlay.
+  $overlay.show();
+  
+  //Get child's alt attribute and set caption
+
+
+});
+
+//When overlay is clicked
+$overlay.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+});
+
+// END of image gallery section
 
 
 
