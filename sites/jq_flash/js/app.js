c8d81d9fc35229a87a367b635b7085fa64bf89ca
Tom from Tomayo Web Design, Portland OR USA <tomayo.web.design@gmail.com>
2015-12-26 19:14:42 -0800

    Abstraced to document.ready. Abstracted methods where applicable. Broke everything. I've got a new idea.

--- sites/jq_flash/js/app.js
+++ sites/jq_flash/js/app.js
@@ -28,45 +28,109 @@
   });
 // END of menu section 
 
-// START of flashcard section 
-  var $displayedCard = $('<div class="displayed_flashcard">');
+var $displayedCard;
 
-  //Add displayedCard to the html body
-  $("body").append($displayedCard);
+$(document).ready(function() {
+  initializeHandlers();
+});
 
-  //Capture the click event on a link to a flashcard list item
-  $("#flashcardCollection li").click(function(event){
-    event.preventDefault();
+function initializeHandlers() {
+  // Remove all handlers from the set of all flashcards 
+  $("#flashcardCollection li").off(); 
+  $("#prevC").off(); 
+  $("#currentC").off(); 
+  $("#nextC").off(); 
+
+  // Function to create html elements for the chosen/clicked on flashcard
+  function buildFlashcard() {
+    $displayedCard = $('<div id="currentC" class="displayed_flashcard">');
 
     //If a previous li element exists i.e. another flashcard is in the list before the current card...
     if($(this).prev().is('li')) {
       // Show the previous/last button
-      $displayedCard.append("<div class='previous_card'>Prev</div>");
-    } else {}
+      $displayedCard.append("<div id='prevC' class='previous_card'>Prev</div>");
+    } else { /*Do nothing*/ }
 
     // Set the html with the clicked-on card's text inside it
     $displayedCard.append($(this).text());
 
-    //If another li element exists i.e. another flashcard is in the list after the current cart...
+    //If another li element exists i.e. another flashcard is in the list after the current card...
     if($(this).next().is('li')) {
       // Show the next/right button
-      $displayedCard.append("<div class='next_card'>Next</div></div>"); //Second ending div for displayed_flashcard opening
+      $displayedCard.append("<div id='nextC' class='next_card'>Next</div></div>"); //Second ending div for displayed_flashcard opening
     } else { $displayedCard.append("</div>"); } //Closing div for displayed_flashcard opening
 
+    //Run next function -- append newly created div to the html body
+    appendToBody();
+  }
+
+  function appendToBody() {
+    //Add displayedCard to the html body
+    $("body").append($displayedCard);  
+
+    //Run next function -- add event listeners
+    flashcardListeners();
+  }
+
+  // Add event handler for flashcard from the set of all cards
+  $("#flashcardCollection li").on("click", function(event) {
+    event.preventDefault();
+    //When displayedcard is clicked on
+    $displayedCard.on("click", function() {
+      //Hide the displayedCard
+      $displayedCard.hide();
+      //Remove the contents of the variable to prevent accretion.
+      $displayedCard.empty();
+    });
     //Show the displayed card.
     $displayedCard.show();
   });
 
-  //TODO: Button on the displayed card that "flips" the card to alternate hide/show the word/definition
-
-  //When displayedcard is clicked
-  $displayedCard.click(function(){
-    //Hide the displayedCard
-    $displayedCard.hide();
-    $displayedCard.empty();
+  // Add event handler for previous card button prevC 
+  $("#prevC").on("click", function(event) {
+    event.preventDefault();
+    //Show the previous card
+    buildFlashcard(event.prev());
   });
-// END of flashcard section
 
+  // Add event handler for previous card button currently opened card currentC
+  $("#currentC").on("click", function(event) {
+    event.preventDefault();
+    //Show the other side/flip the card
+    (this).('.question').hide();
+    (this).('.answer').show();
+  });
+
+                        $('.question').on('click', function() {
+                          $('.answer p').show();
+                        });
+
+
+  // Add event handler for next card button nextC
+  $("#nextC").on("click", function(event) {
+    event.preventDefault();
+    //Show the next card
+    buildFlashcard(event.next());
+  });
+}
 
 //TODO: Section for adding a new card, fields for word, definition, and submit/set button
 
+
+/* ORDER: 
+Build card  
+  Open div with class and/or id
+  Previous button (if needed)
+  Flashcard
+    Back of flashcard
+  Next button (if needed)
+  Closing div tag
+
+Click binding 
+  Previous button (if needed)
+  Flip button
+  Next button
+  On-screen but off flashcard to "put it down"
+
+Append to body
+*/
