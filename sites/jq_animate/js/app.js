97fa207339b37ff778b7cf2bed07809063872b8e
Tom from Tomayo Web Design, Portland OR USA <tomayo.web.design@gmail.com>
2016-01-03 22:14:32 -0800

    Added a new jQuery animate site to play with different animation methods within jQ.

--- sites/jq_animate/js/app.js
+++ sites/jq_animate/js/app.js
@@ -1,3 +1,16 @@
+function animateHorizontal() {
+  $('.horizontal_div:not(:first-child)').children().animate({
+    height: 'toggle'
+  });
+  eventBindings();
+}
+
+function animateVertical() {
+  $('.vertical_div:not(:first-child)').children().animate({
+    width: 'toggle'
+  });
+  eventBindings();
+}
 
 function getNumbers() {
   return 1 + Math.floor(Math.random() * 255);
@@ -15,6 +28,13 @@ function eventBindings() {
   // The next button increments the selectedCardNumber up by one
   $('.color_card').off();  
   $('.color_card').on("click", changeColor);
+
+  // The hide it button toggles the content from showing or not
+  $('.horizontal_span').off();
+  $('.horizontal_span').on('click', animateHorizontal);
+
+  $('.vertical_span').off();
+  $('.vertical_span').on('click', animateVertical);
 }
 
 $(document).ready(function() {
