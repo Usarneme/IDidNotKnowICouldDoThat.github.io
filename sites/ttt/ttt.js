973b72e712dcce62de07fef9651d9aba7d7e7b4e
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2016-02-28 22:19:04 -0800

    Core functionality is now working. TODO: Fix reset button after game is over. Fix game-over checking function--particularly it using the currentPlayer variable when checking for a win.

--- sites/ttt/ttt.js
+++ sites/ttt/ttt.js
@@ -1,43 +1,157 @@
 // Variable to track whose turn it is
-var isXTurn = true;
+var currentPlayer = 'x';
 
 function changeTurn() {
-	if (isXTurn === true) { 
-		isXTurn = false;
+	if (currentPlayer === 'x') { 
+		currentPlayer = 'y';
 	} else { 
-		isXTurn = true;
+		currentPlayer = 'x';
 	}
 } // end of changeTurn function
 
 function updateSquare(e) { 
 	var element = document.getElementById(e.target.id);
-	if (isXTurn) {
-		element.firstChild.innerHTML = 'x';
-		element.className = "btn btn-default col-xs-4 disabled";
-		changeTurn();
+	if (e.target.id == 'gameBoard') { //do nothing 
 	} else {
-		element.firstChild.innerHTML = 'o';
-		element.className = "btn btn-default col-xs-4 disabled";
-		changeTurn();
+		if (currentPlayer === 'x') {
+			element.firstChild.innerHTML = 'x';
+			element.className = "btn btn-default col-xs-4 disabled";
+			document.getElementById('x').className = "btn btn-default col-xs-3 col-xs-offset-3";
+			document.getElementById('o').className = "btn btn-default col-xs-3 selected";
+		} else {
+			element.firstChild.innerHTML = 'o';
+			element.className = "btn btn-default col-xs-4 disabled";
+			document.getElementById('x').className = "btn btn-default col-xs-3 col-xs-offset-3 selected";
+			document.getElementById('o').className = "btn btn-default col-xs-3";
+		}
 	}
+	checkForWin();
+	checkIfBoardIsFull();
+	changeTurn();
 } // end of updateSquare function
+
+function checkIfBoardIsFull() { 
+	var counter = 0;
+	for (i=1; i<10; i++) { 
+		if (document.getElementById(i).firstChild.innerHTML != '') { 
+			counter++;
+		} else { }
+	}
+	if (counter >= 9) { 
+		displayLoss();
+		return;
+	}
+} // end of check if board is full function
+
+function checkForWin() { 
+	// look for diagonal win
+	if ( (document.getElementById('1').firstChild.innerHTML &&
+	document.getElementById('5').firstChild.innerHTML &&
+	document.getElementById('9').firstChild.innerHTML) == currentPlayer ) 
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	if ( (document.getElementById('3').firstChild.innerHTML &&
+	document.getElementById('5').firstChild.innerHTML &&
+	document.getElementById('7').firstChild.innerHTML) == currentPlayer ) 
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	// look for horizontal win
+	if ( (document.getElementById('1').firstChild.innerHTML &&
+	document.getElementById('2').firstChild.innerHTML &&
+	document.getElementById('3').firstChild.innerHTML) == currentPlayer ) 
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	if ( (document.getElementById('4').firstChild.innerHTML &&
+	document.getElementById('5').firstChild.innerHTML &&
+	document.getElementById('6').firstChild.innerHTML) == currentPlayer )
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	if ( (document.getElementById('7').firstChild.innerHTML &&
+	document.getElementById('8').firstChild.innerHTML &&
+	document.getElementById('9').firstChild.innerHTML) == currentPlayer )
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	// look for vertical win 
+	if ( (document.getElementById('1').firstChild.innerHTML &&
+	document.getElementById('4').firstChild.innerHTML &&
+	document.getElementById('7').firstChild.innerHTML) == currentPlayer ) 
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	if ( (document.getElementById('2').firstChild.innerHTML &&
+	document.getElementById('5').firstChild.innerHTML &&
+	document.getElementById('8').firstChild.innerHTML) == currentPlayer ) 
+	{ 
+		displaySuccess();
+		return;
+	} else {}
+
+	if ( (document.getElementById('3').firstChild.innerHTML &&
+	document.getElementById('6').firstChild.innerHTML &&
+	document.getElementById('9').firstChild.innerHTML) == currentPlayer )
+ 	{ 
+		displaySuccess();
+		return;
+	} else {}
+} // end of checkForWin function
+
+function displaySuccess() { 
+	var theElement = document.getElementById('victory');
+	theElement.innerHTML += "<p>" + currentPlayer.toUpperCase() + " has won! Great job, " + currentPlayer.toUpperCase() + "!</p>";
+	theElement.className = "success";
+} // end of displaySuccess function
+
+function displayLoss() { 
+	var theElement = document.getElementById('loss');
+	theElement.innerHTML += "<p>Nobody has won.</p>";
+	theElement.className = "success";
+} // end of displayLoss function
 
 $(document).ready(function() {
 
 	document.getElementById('x').addEventListener('click', function(e) {
 		document.getElementById('o').className = "btn btn-default col-xs-3";
 		document.getElementById('x').className = "btn btn-default col-xs-3 col-xs-offset-3 selected";
-		isXTurn = true;
+		changeTurn();
 	}); 
 
 	document.getElementById('o').addEventListener('click', function(e) {
 		document.getElementById('x').className = "btn btn-default col-xs-3 col-xs-offset-3";
 		document.getElementById('o').className = "btn btn-default col-xs-3 selected";
-		isXTurn = false;
+		changeTurn();
 	}); 
 
-	document.getElementById('gameBoard').addEventListener( 'click', function(e) { 
+	document.getElementById('gameBoard').addEventListener('click', function(e) { 
+		if (e.target.id == 'gameBoard') { 
+			// do nothing
+		} else { 
 		updateSquare(e);
+		}
 	}); 
+
+	document.getElementById('resetWin').addEventListener('click', function(e) { 
+		location.reload(); 
+	});
+
+	document.getElementById('resetLoss').addEventListener('click', function(e) { 
+		location.reload();
+	});
 
 }); // end of document.ready
