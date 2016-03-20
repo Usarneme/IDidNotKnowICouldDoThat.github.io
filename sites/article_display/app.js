c3097384f8c362dc02759efd7993810938455a34
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2016-03-20 11:11:51 -0700

    Added another book, book4 from Meditations.

--- sites/article_display/app.js
+++ sites/article_display/app.js
@@ -1,5 +1,6 @@
 // String holder for json data
-var json2, json3;
+var json2, json3, json4;
+var coin = 0;
 
 // Parse the file contents so I can work with them
 var client2 = new XMLHttpRequest();
@@ -17,11 +18,30 @@ client3.onreadystatechange = function() {
 }
 client3.send();
 
+// Parse the file contents so I can work with them
+var client4 = new XMLHttpRequest();
+client4.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book4.json');
+client4.onreadystatechange = function() {
+	json4 = $.parseJSON(client4.responseText)
+}
+client4.send();
+
 // Event listener to request a random article when the button is clicked
 document.getElementById('getNewArticle').addEventListener('click', function(e) {
 	e.preventDefault();
 	e.stopPropagation();
-	var rand = Math.floor((Math.random() * json2.length));
-	document.getElementById('madlib_holder').textContent = json2[rand][(rand+1)];
+	if (coin === 0) {
+		coin = 2;
+		var rand = Math.floor((Math.random() * json2.length));
+		document.getElementById('madlib_holder').textContent = json2[rand][(rand+1)];
+	} else if (coin > 1) {
+			coin = 1;
+			var rand = Math.floor((Math.random() * json3.length));
+			document.getElementById('madlib_holder').textContent = json3[rand][(rand+1)];
+			} else {
+					coin = 0;
+					var rand = Math.floor((Math.random() * json4.length));
+					document.getElementById('madlib_holder').textContent = json4[rand][(rand+1)];
+			}
 	document.getElementById('madlib_holder').className = "";
 });
