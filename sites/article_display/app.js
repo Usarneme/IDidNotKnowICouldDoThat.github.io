176b388897c3e1f4dd58820fc1b8e168cf99a65d
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2016-03-20 10:49:33 -0700

    Moved background image from web to this project folder. Added another book from Meditations.

--- sites/article_display/app.js
+++ sites/article_display/app.js
@@ -1,19 +1,27 @@
 // String holder for json data
-var json = "";
+var json2, json3;
 
 // Parse the file contents so I can work with them
-var client = new XMLHttpRequest();
-client.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book2.json');
-client.onreadystatechange = function() {
-	json = $.parseJSON(client.responseText)
+var client2 = new XMLHttpRequest();
+client2.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book2.json');
+client2.onreadystatechange = function() {
+	json2 = $.parseJSON(client2.responseText)
 }
-client.send();
+client2.send();
+
+// Parse the file contents so I can work with them
+var client3 = new XMLHttpRequest();
+client3.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book3.json');
+client3.onreadystatechange = function() {
+	json3 = $.parseJSON(client3.responseText)
+}
+client3.send();
 
 // Event listener to request a random article when the button is clicked
 document.getElementById('getNewArticle').addEventListener('click', function(e) {
 	e.preventDefault();
 	e.stopPropagation();
-	var rand = Math.floor((Math.random() * json.length));
-	document.getElementById('madlib_holder').textContent = json[rand][(rand+1)];
+	var rand = Math.floor((Math.random() * json2.length));
+	document.getElementById('madlib_holder').textContent = json2[rand][(rand+1)];
 	document.getElementById('madlib_holder').className = "";
 });
