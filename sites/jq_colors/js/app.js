b48de245ae522c8c2f5d25ff5bf58baf85b8c8bd
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-02-02 12:45:02 -0800

    Updated jq_colors to include a pure JavaScript version (not using any jQuery).

--- sites/jq_colors/js/app.js
+++ sites/jq_colors/js/app.js
@@ -1,30 +1,38 @@
 /*
  the JavaScript version
- TODO
 */
+
+const boxes = 1032;
 
 function getNumbers() {
   return 1 + Math.floor(Math.random() * 255);
-};
+}
 
 function changeColor() {
-  $('.color_card').each(function() {
-    $(this).css('background-color', 'rgba('+ getNumbers() + ',' + getNumbers() + ',' + getNumbers() + ',0.9)');
-  });
+  var wrap = document.getElementById('wrapper');
+  var wrapChildren = wrap.childNodes;
+
+  for (var i=0; i<wrapChildren.length; i++) {
+    var color = 'rgba('+ getNumbers() +','+ getNumbers() +','+ getNumbers() +',0.9)';
+    wrapChildren[i].style.backgroundColor = color;
+  }
   eventBindings();
-};
+}
+
+function makeBoxes() {
+  var wrap = document.getElementById('wrapper');
+
+  for(var i=1; i<=boxes; i++) {
+    var current = document.createElement('div');
+    current.id = i;
+    current.className = 'color_card';
+    wrap.appendChild(current);
+  }
+}
 
 function eventBindings() {
-  $('.color_card').off();
-  $('.color_card').on("click", changeColor);
-};
+  var wrap = document.getElementById('wrapper');
 
-$(document).ready(function() {
-  for(var i=1; i<=1032; i++) {
-    $('div.theWrapper').append('<div id=\"'+i+'\"></div>');
-  }
-  $('div.theWrapper').children().addClass('color_card');
-
-  // Begin with a random color
-  changeColor();
-});
+  wrap.onclick = '';
+  wrap.onclick = changeColor;
+}
