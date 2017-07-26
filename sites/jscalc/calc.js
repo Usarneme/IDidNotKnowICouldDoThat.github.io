4532a4fd0ed4fd63a66f624cf5cfbb9baa2bf81c
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-07-25 23:20:19 -0700

    Updated Calculator application to use ES6 const/let and arrow functions. Refactored to adjust variable names for readability. Removed Bootstrap styling and added a CDN to normalize.css. Added custom styling.

--- sites/jscalc/calc.js
+++ sites/jscalc/calc.js
@@ -2,72 +2,59 @@
 * global vars *
 **************/
 
-var displayMessage = ''; // what shows in the result field
-var sigFig = 2; // how many decimal places in the result
+let displayMessage = string; // what shows in the result field
 
 /********************************************************
 * functions to update the display and calculate answers *
 ********************************************************/
 
-function displayElement(el) {
-	if (el == '=') {
+displayElement = (elementId) => {
+	if (elementId == 'equals') {
 		displayEquals();
-	} else if (el == 'clear') {
+	} else if (elementId == 'clear') {
 		clearAll();
-	} else if (el == 'backspace') {
+	} else if (elementId == 'backspace') {
 		backSpace();
-	} else if (el == 'numpad') {
+	} else if (elementId == 'calculator') {
 		// do nothing if the user clicks on part of the screen that is not on a button
-	} else if (el == 'dec0') {
-		sigFig = 0;
-	} else if (el == 'dec1') {
-		sigFig = 1;
-	} else if (el == 'dec2') {
-		sigFig = 2;
-	} else if (el == 'dec3') {
-		sigFig = 3;
-	} else if (el == 'dec4') {
-		sigFig = 4;
-	} else if (el == 'dec5') {
-		sigFig = 5;
 	} else {
-	    displayMessage += el;
-	    var elMsg = document.getElementById('answer');
+	    displayMessage += elementId;
+	    const elMsg = document.getElementById('answer');
 	    elMsg.textContent = displayMessage;
 	}
 }
 
-function clearAll() {
+clearAll = () => {
 	displayMessage = '';
-	var elMsg = document.getElementById('answer');
+	const elMsg = document.getElementById('answer');
 	elMsg.textContent = displayMessage;
 }
 
-function backSpace() {
+backSpace = () => {
 	displayMessage = displayMessage.substring(0, (displayMessage.length-1));
-	var elMsg = document.getElementById('answer');
+	const elMsg = document.getElementById('answer');
 	elMsg.textContent = displayMessage;
 }
 
-function displayEquals() {
-	var resultArray, result; // to contain the left and right side of the operation and result
+displayEquals = () => {
+	let resultArray, result; // to contain the left and right side of the operation and result
 	if (displayMessage.indexOf('+') != -1) {
 		resultArray = displayMessage.split('+');
-		result = (parseFloat(resultArray[0]) + parseFloat(resultArray[1])).toFixed(sigFig);
+		result = (parseFloat(resultArray[0]) + parseFloat(resultArray[1]));
 	} else if (displayMessage.indexOf('-') != -1) {
 		resultArray = displayMessage.split('-');
-		result = (parseFloat(resultArray[0]) - parseFloat(resultArray[1])).toFixed(sigFig);
+		result = (parseFloat(resultArray[0]) - parseFloat(resultArray[1]));
 	} else if (displayMessage.indexOf('*') != -1) {
 		resultArray = displayMessage.split('*');
-		result = (parseFloat(resultArray[0]) * parseFloat(resultArray[1])).toFixed(sigFig);
+		result = (parseFloat(resultArray[0]) * parseFloat(resultArray[1]));
 	} else if (displayMessage.indexOf('/') != -1) {
 		resultArray = displayMessage.split('/');
-		result = (parseFloat(resultArray[0]) / parseFloat(resultArray[1])).toFixed(sigFig);
+		result = (parseFloat(resultArray[0]) / parseFloat(resultArray[1]));
 	} else {
 		resultArray = displayMessage.split('%');
-		result = (parseFloat(resultArray[0]) % parseFloat(resultArray[1])).toFixed(sigFig);
+		result = (parseFloat(resultArray[0]) % parseFloat(resultArray[1]));
 	}
-	var elMsg = document.getElementById('answer');
+	const elMsg = document.getElementById('answer');
 	elMsg.textContent += ' = '+result;
 }
 
@@ -75,10 +62,8 @@ function displayEquals() {
 * event listener for button clicks      *
 ****************************************/
 
-var answerEl = document.getElementById('numpad');
-answerEl.addEventListener('click', function(event) {
-
+const answerEl = document.getElementById('calculator');
+answerEl.addEventListener('click', (event) => {
     displayElement(event.target.id)
     event.stopPropagation()
-
 }, false);
