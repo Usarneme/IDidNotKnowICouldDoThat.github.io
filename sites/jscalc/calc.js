ff5540eca2e86f62fd31690d91fbf95456a831b2
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-07-26 10:07:12 -0700

    Adjusted JS Calculator app for readability by displaying the result on a newline and limiting length so there are no overflow issues. Added a slider for significant figures/how many decimal places are to be shown in the result.

--- sites/jscalc/calc.js
+++ sites/jscalc/calc.js
@@ -2,7 +2,8 @@
 * global vars *
 **************/
 
-let displayMessage = ''; // what shows in the result field
+let displayMessage = '', // what shows in the result field
+	decimalPlaces = 2; // to hold how many decimal places show in the result, default 2
 
 /********************************************************
 * functions to update the display and calculate answers *
@@ -10,14 +11,28 @@ let displayMessage = ''; // what shows in the result field
 
 displayElement = (elementId) => {
 	if (elementId == 'equals') {
+		// make the operator and number buttons un-clickable
+		disableNumbers();
+		disableOperators();
+		// update the display with the answer
 		displayEquals();
 	} else if (elementId == 'clear') {
+		// make operator and number buttons clickable
+		enableOperators();
+		enableNumbers();
+		// clear out the display
 		clearAll();
 	} else if (elementId == 'backspace') {
 		backSpace();
-	} else if (elementId == 'calculator') {
+	} else if (elementId == '+' || elementId == '-' || elementId == '*' || elementId == '/') {
+		// an operator was clicked...
+		disableOperators();
+		displayMessage += elementId;
+	    const elMsg = document.getElementById('answer');
+	    elMsg.textContent = displayMessage;
+	} else if (elementId == 'calculator' || elementId == 'answer') { 
 		// do nothing if the user clicks on part of the screen that is not on a button
-	} else {
+	} else { // a number was clicked...
 	    displayMessage += elementId;
 	    const elMsg = document.getElementById('answer');
 	    elMsg.textContent = displayMessage;
@@ -31,6 +46,14 @@ clearAll = () => {
 }
 
 backSpace = () => {
+	// If the previous character to be deleted is an operator...
+	if (displayMessage[displayMessage.length-1] == '+' 
+	|| displayMessage[displayMessage.length-1] == '-' 
+	|| displayMessage[displayMessage.length-1] == '*' 
+	|| displayMessage[displayMessage.length-1] == '/') {
+		// re-enable the operators
+		enableOperators();
+	}
 	displayMessage = displayMessage.substring(0, (displayMessage.length-1));
 	const elMsg = document.getElementById('answer');
 	elMsg.textContent = displayMessage;
@@ -40,30 +63,65 @@ displayEquals = () => {
 	let resultArray, result; // to contain the left and right side of the operation and result
 	if (displayMessage.indexOf('+') != -1) {
 		resultArray = displayMessage.split('+');
-		result = (parseFloat(resultArray[0]) + parseFloat(resultArray[1]));
+		result = (parseFloat(resultArray[0]) + parseFloat(resultArray[1])).toFixed(decimalPlaces);
 	} else if (displayMessage.indexOf('-') != -1) {
 		resultArray = displayMessage.split('-');
-		result = (parseFloat(resultArray[0]) - parseFloat(resultArray[1]));
+		result = (parseFloat(resultArray[0]) - parseFloat(resultArray[1])).toFixed(decimalPlaces);
 	} else if (displayMessage.indexOf('*') != -1) {
 		resultArray = displayMessage.split('*');
-		result = (parseFloat(resultArray[0]) * parseFloat(resultArray[1]));
+		result = (parseFloat(resultArray[0]) * parseFloat(resultArray[1])).toFixed(decimalPlaces);
 	} else if (displayMessage.indexOf('/') != -1) {
 		resultArray = displayMessage.split('/');
-		result = (parseFloat(resultArray[0]) / parseFloat(resultArray[1]));
-	} else {
-		resultArray = displayMessage.split('%');
-		result = (parseFloat(resultArray[0]) % parseFloat(resultArray[1]));
-	}
+		result = (parseFloat(resultArray[0]) / parseFloat(resultArray[1])).toFixed(decimalPlaces);
+	} 
 	const elMsg = document.getElementById('answer');
-	elMsg.textContent += ' = '+result;
+	// the answer field can hold about 19 characters, so limit the result to that
+	elMsg.textContent += ' = '+result.toString().substring(0,20);
+	document.getElementById('equals').disabled = true;
+}
+
+disableOperators = () => {
+	const operators = document.querySelectorAll('.operator');
+	for(let i=0; i<operators.length; i++) {
+		operators[i].disabled = true;
+	}
+}
+
+enableOperators = () => {
+	const operators = document.querySelectorAll('.operator');
+	for(let i=0; i<operators.length; i++) {
+		operators[i].disabled = false;
+	}
+	document.getElementById('equals').disabled = false;
+}
+
+disableNumbers = () => {
+	const numbers = document.querySelectorAll('.num');
+	for(let i=0; i<numbers.length; i++) {
+		numbers[i].disabled = true;
+	}
+}
+
+enableNumbers = () => {
+	const numbers = document.querySelectorAll('.num');
+	for(let i=0; i<numbers.length; i++) {
+		numbers[i].disabled = false;
+	}
 }
 
 /****************************************
-* event listener for button clicks      *
+* event listeners                       *
 ****************************************/
 
 const answerEl = document.getElementById('calculator');
 answerEl.addEventListener('click', (event) => {
-    displayElement(event.target.id)
+    displayElement(event.target.id),
     event.stopPropagation()
+}, false);
+
+const sigFigInput = document.getElementById('sigFigInput'),
+	sigFig = document.getElementById('sigFig');
+sigFigInput.addEventListener('input', () => {
+	sigFig.textContent = sigFigInput.value+' decimal places';
+	decimalPlaces = sigFigInput.value;
 }, false);
