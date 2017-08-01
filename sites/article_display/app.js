3fbfe4ae1287fcbb2ee2bfff526c1038e88ba66c
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-07-31 18:00:35 -0700

    Adjusted article display application for better readability in code, style changes and adjustments for different screen sizes, slight refactoring to reduce code duplication (DRY).

--- sites/article_display/app.js
+++ sites/article_display/app.js
@@ -1,12 +1,11 @@
 // String holder for json data
-var json2, json3, json4;
-var coin = 0;
+var json2, json3, json4, coin = 0;
 
 // Parse the file contents so I can work with them
 var client2 = new XMLHttpRequest();
 client2.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book2.json');
 client2.onreadystatechange = function() {
-	json2 = $.parseJSON(client2.responseText)
+	json2 = $.parseJSON(client2.responseText);
 }
 client2.send();
 
@@ -14,7 +13,7 @@ client2.send();
 var client3 = new XMLHttpRequest();
 client3.open('GET', 'https://raw.githubusercontent.com/IDidNotKnowICouldDoThat/IDidNotKnowICouldDoThat.github.io/master/sites/article_display/book3.json');
 client3.onreadystatechange = function() {
-	json3 = $.parseJSON(client3.responseText)
+	json3 = $.parseJSON(client3.responseText);
 }
 client3.send();
 
@@ -27,23 +26,22 @@ client4.onreadystatechange = function() {
 client4.send();
 
 // Event listener to request a random article when the button is clicked
-document.getElementById('get_new_article').addEventListener('click', function(e) {
+document.getElementById('get-new-article-button').addEventListener('click', function(e) {
+	var element = document.getElementById('article');
 	e.preventDefault();
 	e.stopPropagation();
 	if (coin === 0) {
 		coin = 2;
-		var rand = Math.floor((Math.random() * json2.length));
-		document.getElementById('madlib_holder').textContent = json2[rand][(rand+1)];
+		var $rand = Math.floor((Math.random() * json2.length));
+		element.textContent = json2[$rand][$rand+1];
 	} else if (coin > 1) {
 			coin = 1;
-			var rand = Math.floor((Math.random() * json3.length));
-			document.getElementById('madlib_holder').textContent = json3[rand][(rand+1)];
+			var $rand = Math.floor((Math.random() * json3.length));
+			element.textContent = json3[$rand][$rand+1];
 			} else {
-					coin = 0;
-					var rand = Math.floor((Math.random() * json4.length));
-					document.getElementById('madlib_holder').textContent = json4[rand][(rand+1)];
+				coin = 0;
+				var $rand = Math.floor((Math.random() * json4.length));
+				element.textContent = json4[$rand][$rand+1];
 			}
-	console.log('Setting madlib_holder to visible.');
-	document.getElementById('madlib_holder').className = "";
-	document.getElementById('header').className = "after";
+	element.className = 'article';
 });
