c46b3242b5f4fbb0bdafb9e128c47d01385cec0a
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-08-12 13:33:26 -0700

    Removed extraneous divs from Portfolio site links to other Projects to make them easier to click on mobile and to animate more consistently after testing on various screen sizes. Adjusted Tic Tac Toe game from divs to buttons for better user interaction on mobile devices. Adjusted the behavior by disabling a button after it has been clicked once and disabling all buttons after the game has been won.

--- sites/ttt/ttt.js
+++ sites/ttt/ttt.js
@@ -3,16 +3,23 @@ var currentPlayer = 'x';
 
 function updateSquare(e) { 
 	var element = document.getElementById(e.target.id);
-	if (e.target.id == 'gameBoard') { //do nothing 
+	// if the Gameboard (not a button on the board) is clicked...
+	if (e.target.id == 'gameBoard') { 
+		// ...do nothing 
+	// if the button clicked is already disabled
+	} else if ( element.classList.toString().indexOf('disabled') != -1 ) {
+		// ...do nothing
 	} else {
 		if (currentPlayer == 'x') {
-			element.firstChild.innerHTML = 'x';
+			element.innerHTML = 'x';
 			element.className = "btn btn-default col-xs-4 disabled";
+			element.disabled = true;
 			document.getElementById('x').className = "col-xs-1 col-xs-offset-5";
 			document.getElementById('o').className = "col-xs-1 selected";
 		} else {
-			element.firstChild.innerHTML = 'o';
+			element.innerHTML = 'o';
 			element.className = "btn btn-default col-xs-4 disabled";
+			element.disabled = true;
 			document.getElementById('x').className = "col-xs-1 col-xs-offset-5 selected";
 			document.getElementById('o').className = "col-xs-1";
 		}
@@ -32,8 +39,8 @@ function changeTurn() {
 
 function checkIfBoardIsFull() { 
 	var counter = 0;
-	for (i=1; i<10; i++) { 
-		if (document.getElementById(i).firstChild.innerHTML != '') { 
+	for (var i=1; i<10; i++) { 
+		if (document.getElementById(i).innerHTML != '') { 
 			counter++;
 		} else { }
 	}
@@ -46,9 +53,9 @@ function checkIfBoardIsFull() {
 function checkForWin(currPlayer) { 
 	// look for diagonal win
 	if ( 
-	(document.getElementById('1').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('5').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('9').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('1').innerHTML == currPlayer) &&
+	(document.getElementById('5').innerHTML == currPlayer) &&
+	(document.getElementById('9').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('1').style.background = '#7cff00';
@@ -59,9 +66,9 @@ function checkForWin(currPlayer) {
 	} else {}
 
 	if ( 
-	(document.getElementById('3').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('5').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('7').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('3').innerHTML == currPlayer) &&
+	(document.getElementById('5').innerHTML == currPlayer) &&
+	(document.getElementById('7').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('3').style.background = '#7cff00';
@@ -73,9 +80,9 @@ function checkForWin(currPlayer) {
 
 	// look for horizontal win
 	if ( 
-	(document.getElementById('1').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('2').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('3').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('1').innerHTML == currPlayer) &&
+	(document.getElementById('2').innerHTML == currPlayer) &&
+	(document.getElementById('3').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('1').style.background = '#7cff00';
@@ -86,9 +93,9 @@ function checkForWin(currPlayer) {
 	} else {}
 
 	if ( 
-	(document.getElementById('4').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('5').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('6').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('4').innerHTML == currPlayer) &&
+	(document.getElementById('5').innerHTML == currPlayer) &&
+	(document.getElementById('6').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('4').style.background = '#7cff00';
@@ -99,9 +106,9 @@ function checkForWin(currPlayer) {
 	} else {}
 
 	if ( 
-	(document.getElementById('7').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('8').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('9').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('7').innerHTML == currPlayer) &&
+	(document.getElementById('8').innerHTML == currPlayer) &&
+	(document.getElementById('9').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('7').style.background = '#7cff00';
@@ -113,9 +120,9 @@ function checkForWin(currPlayer) {
 
 	// look for vertical win 
 	if ( 
-	(document.getElementById('1').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('4').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('7').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('1').innerHTML == currPlayer) &&
+	(document.getElementById('4').innerHTML == currPlayer) &&
+	(document.getElementById('7').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('1').style.background = '#7cff00';
@@ -126,9 +133,9 @@ function checkForWin(currPlayer) {
 	} else {}
 
 	if ( 
-	(document.getElementById('2').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('5').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('8').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('2').innerHTML == currPlayer) &&
+	(document.getElementById('5').innerHTML == currPlayer) &&
+	(document.getElementById('8').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('2').style.background = '#7cff00';
@@ -139,9 +146,9 @@ function checkForWin(currPlayer) {
 	} else {}
 
 	if ( 
-	(document.getElementById('3').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('6').firstChild.innerHTML == currPlayer) &&
-	(document.getElementById('9').firstChild.innerHTML == currPlayer) 
+	(document.getElementById('3').innerHTML == currPlayer) &&
+	(document.getElementById('6').innerHTML == currPlayer) &&
+	(document.getElementById('9').innerHTML == currPlayer) 
 	) 
 	{
 		document.getElementById('9').style.background = '#7cff00';
@@ -153,35 +160,45 @@ function checkForWin(currPlayer) {
 } // end of checkForWin function
 
 function displaySuccess() { 
+	// If the victory div is displayed
 	if (document.getElementById('victory').className != "hidden") {
-		// do nothing since victory was previously achieved in this game
+		// do nothing as the game was won previously
 	} else {
 		var theElement = document.getElementById('victory');
-		theElement.innerHTML = currentPlayer.toUpperCase() + " has won! Great job, " + currentPlayer.toUpperCase() + "!";
+		theElement.innerHTML = '<p>'+currentPlayer.toUpperCase() + " has won! Great job, " + currentPlayer.toUpperCase() + "!"+'</p>';
 		theElement.className = "jumbotron success";
 
 		var resetWin = document.getElementById('resetWin');
+		theElement.appendChild(resetWin);
 		resetWin.className = "btn btn-default reset";
+
+		// Disabled all of the remaining squares in the gameboard
+		var buttons = document.getElementById('gameBoard').children;
+		console.log('displaySuccess func. Selected element: '+buttons);
+		for (var i=0; i<buttons.length; i++) {
+			buttons[i].disabled = true;
+		}
+
 		resetWin.addEventListener('click', function(e) { 
+			// TODO Reset values without reloading the page
 			location.reload(false);
 		}); // end of event listener
 	} // end of else
 } // end of displaySuccess function
 
 function displayLoss() { 
-	if (document.getElementById('victory').className != "hidden") {
-		// do nothing since victory was achieved with a full game board
-	} else {
-		var theElement = document.getElementById('loss');
-		theElement.innerHTML = "Nobody has won.";
-		theElement.className = "jumbotron success";
+	var theElement = document.getElementById('loss');
+	theElement.innerHTML = "<p>Nobody has won.</p>";
+	theElement.className = "jumbotron success";
 
-		var resetLoss = document.getElementById('resetLoss');
-		resetLoss.className = "btn btn-default reset";
-		resetLoss.addEventListener('click', function(e) { 
-			location.reload(false);
-		});
-	}
+	var resetLoss = document.getElementById('resetLoss');
+	theElement.appendChild(resetLoss);
+	resetLoss.className = "btn btn-default reset";
+
+	resetLoss.addEventListener('click', function(e) { 
+		// TODO recode reload
+		location.reload(false);
+	});
 } // end of displayLoss function
 
 $(document).ready(function() {
