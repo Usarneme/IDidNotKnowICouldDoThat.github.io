8b1a327155aa87a467f0a9f259413997dd56e692
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-08-25 21:51:46 -0700

    Style adjustments for Task List App. Started mapping the data model needed for storing data using localStorage. Put in buttons (hidden for now) to save and retrieve stored data.

--- sites/task_list_app/js/app.js
+++ sites/task_list_app/js/app.js
@@ -1,14 +1,15 @@
 // An application for tracking tasks through to completion.
 
+// DOM elements used globally
 const taskInput = document.getElementById("new-task"),
-      addButton = document.getElementsByTagName("button")[0], 
+      addButton = document.getElementById("add"), 
       saveButton = document.getElementById("save"),
+      retrieveButton = document.getElementById("retrieve"),
       incompleteTasksHolder = document.getElementById("incomplete-tasks"), 
       completedTasksHolder= document.getElementById("completed-tasks");
 
-// New Task List Item
+// New Task List Item li created and returned
 createNewTaskElement = (taskString) => {
-  //Create List Item
   const listItem = document.createElement("li"),
         label = document.createElement("label"),
         span = document.createElement("span"),
@@ -17,7 +18,7 @@ createNewTaskElement = (taskString) => {
         editButton = document.createElement("button"),
         deleteButton = document.createElement("button");
         
-  //Each element needs modifying  
+  // Modify each element before appending  
   label.innerText = taskString;
   span.innerText = "Complete";
   checkBox.type = "checkbox";
@@ -28,7 +29,7 @@ createNewTaskElement = (taskString) => {
   deleteButton.innerText = "Delete";
   deleteButton.className = "delete";  
   
-  //Each element needs appending
+  // Append each element to the generated list item
   listItem.appendChild(label);
   listItem.appendChild(span);
   listItem.appendChild(checkBox);
@@ -45,12 +46,14 @@ addTask = () => {
   const listItem = createNewTaskElement(taskInput.value);
   //Append listItem to incompleteTasksHolder
   incompleteTasksHolder.appendChild(listItem);
+  //Bind events to the newly created item (edit/delete/completed)
   bindTaskEvents(listItem, taskCompleted);  
+  //Clear out the Add new input field
   taskInput.value = "";
 }
 
 //Edit an existing task
-const editTask = function() {
+editTask = function() {
   const listItem = this.parentNode,
         editInput = listItem.querySelector("input[type=text"),
         label = listItem.querySelector("label"),
@@ -63,30 +66,29 @@ const editTask = function() {
     editInput.value = '';
     editInput.disabled = true;
     editButton.innerText = 'Edit';
-    console.log('Saving');
+//    console.log('Saving');
   } else {
     //input value becomes the label's text
     editInput.value = label.innerText;
     label.innerText = editInput.value+' (editing)...';
     editInput.disabled = false;
     editButton.innerText = 'Save';
-    console.log('Editing');
+//    console.log('Editing');
   }
   //Toggle .editMode on the list item
   listItem.classList.toggle("editMode");
 }
 
 //Delete an existing task
-const deleteTask = function() {
+deleteTask = function() {
   const listItem = this.parentNode,
         ul = listItem.parentNode;
-  
   //Remove the parent list item from the ul
   ul.removeChild(listItem);
 }
 
 //Mark a task as complete
-const taskCompleted = function() {
+taskCompleted = function() {
   //Append the task list item to the #completed-tasks
   const listItem = this.parentNode;
   completedTasksHolder.appendChild(listItem);
@@ -94,52 +96,145 @@ const taskCompleted = function() {
 }
 
 //Mark a task as incomplete
-const taskIncomplete = function() {
+taskIncomplete = function() {
   //Append the task list item to the #incomplete-tasks
   const listItem = this.parentNode;
   incompleteTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 }
 
-const bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+bindTaskEvents = (taskListItem, checkBoxEventHandler) => {
   //select taskListItem's children
   const checkBox = taskListItem.querySelector("input[type=checkbox]"),
         editButton = taskListItem.querySelector("button.edit"),
         deleteButton = taskListItem.querySelector("button.delete");
-
   //bind editTask to edit button
-  editButton.onclick = editTask;
-  
+  editButton.onclick = editTask;  
   //bind deleteTask to delete button
-  deleteButton.onclick = deleteTask;
-  
+  deleteButton.onclick = deleteTask;  
   //bind checkBoxEventHandler to checkbox
   checkBox.onchange = checkBoxEventHandler;
 }
 
 // Get/Save func
-const saveTasks = () => {
+saveTasks = () => {
   const completedTasksArray = [],
         completedListItems = completedTasksHolder.getElementsByTagName('li'),
         incompleteTasksArray = [],
         incompleteListItems = incompleteTasksHolder.getElementsByTagName('li');
 
-  if(completedListItems) {
-    for(let i=0; i<completedListItems.length; i++) {
-      completedTasksArray.push(completedListItems[i].innerHTML);
+  dataModel = {
+    completedTasks: {
+      li: {
+        label: {
+          innerText: 'See the Doctor'
+        },
+        span: {
+          innerText: 'Complete'
+        },  
+        input: {
+          type: 'checkbox',
+          checked: true
+        },
+        input: {
+          type: 'text',
+          disabled: true
+        },
+        button: {
+          class: 'edit',
+          innerText: 'Edit'
+        },
+        button: {
+          class: 'delete',
+          innerText: 'Delete'
+        } 
+      }
+    },
+    incompleteTasks: {
+      // similar
     }
   }
-  localStorage.setItem('completedTasks', completedTasksArray);
 
-  if(incompleteListItems) {
-    for(let i=0; i<incompleteListItems.length; i++) {
-      incompleteTasksArray.push(incompleteListItems[i].innerHTML);
+  if (storageAvailable('localStorage')) {
+    //Store completed tasks (if there is at least 1+ completed list item...)
+    if(completedListItems) {
+      for(let i=0; i<completedListItems.length; i++) {
+        const listItemChildren = {},
+              currentListItem = completedListItems[i];
+        for(element in currentListItem.children) {
+          listItemChildren[currentListItem.localName] = 'TODO SOMETHING';
+          console.log('listItem[element] '+listItem[element]);
+        }
+        completedTasksArray.push(listItem);
+      }
     }
+    console.dir(completedTasksArray);
+    localStorage.setItem('completedTasks', completedTasksArray);
+    //Store incomplete tasks
+    if(incompleteListItems) {
+      for(let i=0; i<incompleteListItems.length; i++) {
+        const listItem = {};
+        for(element in incompleteListItems[i].children) {
+          console.log('element: '+element+' incompleteListItems[i].children[element]: '+incompleteListItems[i].children[element]);
+          listItem[element] = incompleteListItems[i].children[element];
+        }
+        incompleteTasksArray.push(listItem);
+      }
+    }
+    console.dir(incompleteTasksArray);
+    localStorage.setItem('incompleteTasks', incompleteTasksArray);
+  
+    console.dir(localStorage);
+    console.log("Tasks saved to localStorage");
+  } else {
+    //Warn user localstorage is not available
+    alert('Sorry, storage is not available on your device. Try another browser?');
   }
-  localStorage.setItem('incompleteTasks', incompleteTasksArray);
+}
 
-  console.dir(localStorage);
-  console.log("Tasks saved to localStorage");
+retrieveTasks = () => {
+  const completedTasksArray = [],
+        incompleteTasksArray = [];
+
+  if(storageAvailable('localStorage')) {
+    console.log('Retrieveing local storage.');
+    if(localStorage.completedTasks) {
+      completedTasksArray.push(JSON.parse(localStorage.completedTasks));
+      console.dir(completedTasksArray);      
+    }
+    if(localStorage.incompleteTasks) {
+      incompleteTasksArray.push(JSON.parse(localStorage.incompleteTasks));
+      console.dir(incompleteTasksArray);      
+    }
+  } else {
+    //Warn user localstorage is not available
+    alert('Sorry, storage is not available on your device. Try another browser?');
+  }    
+}
+
+// Returns true if localStorage is implemented and available in a given browser
+storageAvailable = (type) => {
+  try {
+    const storage = window[type],
+          x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch(e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0;
+  }
 }
 
 //Set the click handler to the addTask function
@@ -148,14 +243,17 @@ addButton.addEventListener("click", addTask);
 //Save items to localStorage listener
 saveButton.addEventListener("click", saveTasks);
 
-//cycle over incompleteTasksHolder ul list items
+//Retrieve items from localStorage listener
+retrieveButton.addEventListener("click", retrieveTasks);
+
+//Cycle over incompleteTasksHolder ul list items
 for(let i = 0; i < incompleteTasksHolder.children.length; i++) {
-  //bind events to list item's children (taskCompleted)
+  //Bind events to list item's children (taskCompleted)
   bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
 }
 
-//cycle over completedTasksHolder ul list items
+//Cycle over completedTasksHolder ul list items
 for(let i = 0; i < completedTasksHolder.children.length; i++) {
-  //bind events to list item's children (taskIncomplete)
+  //Bind events to list item's children (taskIncomplete)
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
