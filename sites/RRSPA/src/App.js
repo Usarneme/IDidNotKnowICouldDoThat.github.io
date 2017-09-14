1e394389292a5e86e09ff557ac844c718aa4f12f
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-09-13 23:34:28 -0700

    New project started: A Single Page Application (SPA) built using React Router 4. Link added to new site from main portfolio page. Lorem ipsum demonstration SPA page added.

---
+++ sites/RRSPA/src/App.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import {  BrowserRouter as 
+          Router, 
+          Route, 
+          NavLink } from 'react-router-dom'
+import './App.css'
+
+const Home = () => (
+  <div className="home">
+    <h2>Home</h2>
+    <p>Cras facilisis urna ornare ex volutpat, et
+    convallis erat elementum. Ut aliquam, ipsum vitae
+    gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+    metus nec massa. Maecenas hendrerit laoreet augue
+    nec molestie. Cum sociis natoque penatibus et magnis
+    dis parturient montes, nascetur ridiculus mus.</p>
+    <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+    <p>The standard Lorem Ipsum passage, used since the 1500s
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+    <p>1914 translation by H. Rackham
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+  </div>
+)
+
+const About = () => (
+  <div className="about">
+    <h2>About</h2>
+    <div>
+      <p>Mauris sem velit, vehicula eget sodales vitae, rhoncus eget sapien:</p>
+      <ol>
+        <li>Nulla pulvinar diam</li>
+        <li>Facilisis bibendum</li>
+        <li>Vestibulum vulputate</li>
+        <li>Eget erat</li>
+        <li>Id porttitor</li>
+      </ol>
+      <p>The standard Lorem Ipsum passage, used since the 1500s
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      <p>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+      <p>1914 translation by H. Rackham
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+    </div>
+  </div>
+)
+
+const Topic = ({ match }) => {
+  let markup
+  switch(match.params.topicId) {
+    case "translation" :
+    markup = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+    break
+    case "standard" :
+    markup = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+    break
+    case "cicero45bce" :
+    markup = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    break
+    default:
+    markup = "No content loaded at this time. Try one of the nav links above."
+  }
+  return (
+    <div className="topic">
+      <p>{markup}</p>
+    </div>
+  )
+}
+
+const Topics = ({ match }) => (
+  <div className="topics">
+    <h2>Topics</h2>
+    <div className="topics-nav">
+      <NavLink to={`${match.url}/standard`}>Standard</NavLink>
+      <NavLink to={`${match.url}/cicero45bce`}>Cicero 45 BCE</NavLink>
+      <NavLink to={`${match.url}/translation`}>Translation</NavLink>
+    </div>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div className="wrapper">
+      <h1>React Router Single Page Application (SPA)</h1>
+      <div className="main-nav">
+        <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        <NavLink to="/about" activeClassName="active">About</NavLink>
+        <NavLink to="/topics" activeClassName="active">Topics</NavLink>
+      </div>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+
+      <div className="footer">
+        <h4>&copy; 2017</h4>
+      </div>
+    </div>
+  </Router>
+)
+
+export default App
