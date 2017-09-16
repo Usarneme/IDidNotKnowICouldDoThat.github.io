d88a23f72eb8a7adf64c6e68440566d880b92bb0
IDidNotKnowICouldDoThat <jamestcoop@gmail.com>
2017-09-16 13:03:14 -0700

    React Router 4 Single Page Application (SPA) example site moved to being hosted on Surge.sh (to get around relative link issues when hosting custom-routed applications on github pages). Link from main portfolio site updated to point to surge hosting instead of github pages hosted site.

--- sites/RRSPA/src/App.js
+++ sites/RRSPA/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {  BrowserRouter as 
-          Router, 
+import {  BrowserRouter as Router, 
           Route, 
+          Link,
           NavLink,
+          Redirect,
           Switch } from 'react-router-dom'
 import './App.css'
 
@@ -19,6 +20,12 @@ const Home = () => (
     <p>The standard Lorem Ipsum passage, used since the 1500s
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
     <p>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+    <p>1914 translation by H. Rackham
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+    <p>The standard Lorem Ipsum passage, used since the 1500s
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p id="hashLink">Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
     <p>1914 translation by H. Rackham
     "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
@@ -85,27 +92,37 @@ const Topics = ({ match }) => (
   </div>
 )
 
+const FourOhFour = () => ( 
+  <h4 className="home" style={{textAlign:"center", margin:"20px auto"}}>404 Nothing Here, Friendo.</h4> 
+)
+
+// Route render location allows CSS Transition Groups to apply styling to page transitions
 const App = () => (
   <Router>
-    <div className="wrapper">
-      <h1>React Router Single Page Application (SPA)</h1>
-      <div className="main-nav">
-        <NavLink exact to="/" activeClassName="active">Home</NavLink>
-        <NavLink to="/about" activeClassName="active">About</NavLink>
-        <NavLink to="/topics" activeClassName="active">Topics</NavLink>
-      </div>
+    <Route render={({ location }) => (
+      <div className="wrapper">
+        <h1>React Router Single Page Application (SPA)</h1>
+        <div className="main-nav">
+          <NavLink exact to={`/`} activeClassName="active">Home</NavLink>
+          <NavLink to={`/about`} activeClassName="active">About</NavLink>
+          <NavLink to={`/topics`} activeClassName="active">Topics</NavLink>
+        </div>
 
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
-      <Route component={() => ( <h4 className="home" style={{textAlign:"center"}}>404 Nothing Here, Friendo.</h4> )}/>
-    </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/index.html"/>
+        </Route>
+        <Route path="/index.html" exact component={Home}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/topics" component={Topics}/>
+        <Route component={FourOhFour}/>
+      </Switch>
 
-      <div className="footer">
-        <h4>&copy; 2017</h4>
+        <div className="footer">
+          <h4>&copy; 2017</h4>
+        </div>
       </div>
-    </div>
+    )}/>
   </Router>
 )
 
